cee161bdda9b8d6b40ee422fdf323649c1b5a363
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2024-10-15 12:28:04 +0530

    Create rotateMatrixBy90Deg.cpp

---
+++ Day 11/rotateMatrixBy90Deg.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix[0].size();
+        int m = matrix.size();
+
+        for( int i = 0;i < m;i++){
+            for( int j = 0;j < i;j++ ){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        for( int i = 0;i < m;i++ ){
+                reverse(matrix[i].begin(), matrix[i].end());
+        }
+
+        // for( int i = 0;i < m;i++){
+        //     for( int j = 0;j < n;j++ ){
+        //         swap(matrix[i][j], matrix[j][i]);
+        //     }
+        // }
+
+    }
+};
